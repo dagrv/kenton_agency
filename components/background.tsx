c855f7e49a9e7565a83a6ddd0e8cb56db7858879
dagrv <rust.91@icloud.com>
2022-04-12 17:42:08 +0200

    Continuous Video Background

---
+++ components/background.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+
+const Background: React.FC = () => {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">Video Background</div>
+    )
+}
+
+export default Background
